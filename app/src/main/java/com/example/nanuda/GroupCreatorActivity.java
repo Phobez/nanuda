5dac802f594ef16f316eb6acf79759c0209b1128
Spartiazz <Spartiazz@users.noreply.github.com>
2020-11-26 15:29:00 +0900

    Add Participants Name

--- app/src/main/java/com/example/nanuda/GroupCreatorActivity.java
+++ app/src/main/java/com/example/nanuda/GroupCreatorActivity.java
@@ -9,17 +9,52 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 import android.widget.Toolbar;
+import java.util.ArrayList;
 
 public class GroupCreatorActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 int groupId;
+Button save;
+ArrayList<String>addArray = new ArrayList<String>();
+EditText txt;
+ListView show;
+int participantNmb = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
 
         setContentView( R.layout.activity_group_creator );
+        txt = (EditText) findViewById( R.id.ParticipantInput );
+        show= (ListView) findViewById( R.id.participantsListView );
+        save = (Button) findViewById( R.id.addParticipantButton );
+        save.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getInput = txt.getText().toString();
+
+                if(addArray.contains( getInput )){
+                    Toast.makeText( getBaseContext(), "Participant already added ", Toast.LENGTH_LONG ).show();
+                }
+                else if (getInput == null || getInput.trim().equals( "" )){
+                    Toast.makeText( getBaseContext(), "Invalid Name ", Toast.LENGTH_LONG ).show();
+                }
+                else if (participantNmb==20){
+                    Toast.makeText( getBaseContext(), "Maximum participants number reached ", Toast.LENGTH_LONG ).show();
+                }
+                else{
+                    addArray.add(getInput);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(GroupCreatorActivity.this, android.R.layout.simple_list_item_1,addArray  );
+                    show.setAdapter( adapter );
+                    ((EditText)findViewById( R.id.ParticipantInput )).setText( " " );
+
+                }
+            }
+        } );
         EditText groupName = (EditText) findViewById( R.id.groupName );
         Intent intent = getIntent();
         groupId= intent.getIntExtra("groupId", -1);
