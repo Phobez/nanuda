3cb0f54ab6f590ccc3c7c3f049de4f98a67bc171
ndhartanto <ndhartanto@users.noreply.github.com>
2020-11-30 22:28:49 +0900

    update expenses

---
+++ app/src/main/java/com/example/nanuda/ExpenseMain.java
@@ -0,0 +1,14 @@
+package com.example.nanuda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExpenseMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense_main);
+    }
+}
