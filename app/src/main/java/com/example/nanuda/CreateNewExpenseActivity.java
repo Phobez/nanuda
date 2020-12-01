49b183c5a4bccb327865c254c1d7d1c6ed81be96
ndhartanto <ndhartanto@users.noreply.github.com>
2020-12-01 15:02:09 +0900

    expenses update

--- app/src/main/java/com/example/nanuda/CreateNewExpenseActivity.java
+++ app/src/main/java/com/example/nanuda/CreateNewExpenseActivity.java
@@ -12,14 +12,7 @@ public class CreateNewExpenseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_expense);
-    }
-    // idk about this lol
 
-    //get the spinner from the xml.
-    Spinner dropdown = findViewById(R.id.spinner1);
-    //create a list of items for the spinner.
-    String[] whopaid = new String[]{"abia", "nadya", "hugo"};
-    //create an adapter to describe how the items are displayed, adapters are used in several places in android.
-    //There are multiple variations of this, but this is the basic variant.
-    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, whopaid);
+
+    }
 }
--- app/src/main/java/com/example/nanuda/ExpenseMainDisplayActivity.java
+++ app/src/main/java/com/example/nanuda/ExpenseMainDisplayActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ExpenseMain extends AppCompatActivity {
+public class ExpenseMainDisplayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_expense_main);
+        setContentView(R.layout.activity_expense_main_display);
     }
 }
