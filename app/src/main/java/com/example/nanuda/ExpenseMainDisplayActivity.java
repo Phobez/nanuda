018f703e1e1cf0f57eb70ccc938ec9bd5ab500f6
ndhartanto <ndhartanto@users.noreply.github.com>
2020-12-01 20:05:32 +0900

    implemented expenses list
    TODO : integrate with other pieces of code

--- app/src/main/java/com/example/nanuda/ExpenseMainDisplayActivity.java
+++ app/src/main/java/com/example/nanuda/ExpenseMainDisplayActivity.java
@@ -1,14 +1,32 @@
 package com.example.nanuda;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.nanuda.expense.ExpenseAdapter;
+
 public class ExpenseMainDisplayActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
+    String expenseName[], paidBy[], amount[], date[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense_main_display);
+
+        recyclerView = findViewById(R.id.expenses_recycler);
+
+        expenseName= getResources().getStringArray(R.array.expense_name);
+        paidBy = getResources().getStringArray(R.array.paid_by);
+        amount = getResources().getStringArray(R.array.amount);
+        date = getResources().getStringArray(R.array.date_array);
+
+        ExpenseAdapter expenseAdapter = new ExpenseAdapter( this,expenseName, paidBy, amount, date );
+        recyclerView.setAdapter(expenseAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/nanuda/expense/ExpenseAdapter.java
@@ -0,0 +1,63 @@
+package com.example.nanuda.expense;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.nanuda.R;
+
+public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ExpenseViewHolder> {
+
+    String expenseName[], paidBy[], amount[], date[];
+    Context context;
+
+    public ExpenseAdapter(Context ct, String expenseName[], String paidBy[], String amount[], String date[]){
+        context = ct;
+        this.expenseName = expenseName;
+        this.paidBy = paidBy;
+        this.amount = amount;
+        this.date = date;
+    }
+    @NonNull
+    @Override
+    public ExpenseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view =    inflater.inflate(R.layout.expenses_list_item,parent, false);
+        return new ExpenseViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExpenseViewHolder holder, int position) {
+        holder.expenseText.setText(expenseName[position]);
+        holder.paidByText.setText(paidBy[position]);
+        holder.amountText.setText(amount[position]);
+        holder.dateText.setText(date[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return expenseName.length;
+    }
+
+    public class ExpenseViewHolder extends RecyclerView.ViewHolder{
+
+        TextView expenseText, paidByText, amountText, dateText;
+        public ExpenseViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+            expenseText = itemView.findViewById(R.id.expense_name);
+            paidByText = itemView.findViewById(R.id.paid_by);
+            amountText = itemView.findViewById(R.id.amount);
+            dateText = itemView.findViewById(R.id.date);
+
+
+        }
+    }
+}
